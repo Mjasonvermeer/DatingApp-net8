f19cca0c7fe1f9dc85cbf68f36ea31b52c031f4f
Marcel Vermeer <marcel.vermeer@graafschapcollege.nl>
2024-11-20 22:01:32 +0100

    End of section 6

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,23 +2,25 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
-import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, HomeComponent],
+  imports: [RouterOutlet, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit
+{
   private accountSerivce = inject(AccountService);
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
     this.setCurrentUser();
   }
 
-  setCurrentUser() {
+  setCurrentUser()
+  {
     const userString = localStorage.getItem('user');
     if (!userString) return;
     const user = JSON.parse(userString);
